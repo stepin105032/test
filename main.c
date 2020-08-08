ff4fb24ead0a7c17c05ca15d1d1ecf24877f65ba
stepin105032 <69100575+stepin105032@users.noreply.github.com>
2020-08-08 10:41:15 +0530

    example code

---
+++ main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{int n,m;
+    printf("enter number");
+    scanf("%d",&n);
+    m=(n*(n+1))/2;
+    printf("\n%d",m);
+
+    return 0;
+}
